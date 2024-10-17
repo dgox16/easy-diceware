9c9e517f76209abb8753d16919066caf89efccdc
Diego Gomez <Diego.Gomez@pilsac.com>
2024-10-17 10:18:36 -0600

    Se creo la tabla/modelo para las palabras en espanol

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,3 +8,6 @@ Route::controller(UserController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
 });
+Route::middleware('auth:sanctum')->get("/user", function (Request $request) {
+    return $request->user();
+});
