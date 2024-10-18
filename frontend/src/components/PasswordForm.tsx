c616a27bdcc5c9d08225f36c993235d32c1905e5
Diego Gomez <Diego.Gomez@pilsac.com>
2024-10-18 09:11:42 -0600

    Se agregaron las opciones de delimitadores

--- frontend/src/components/PasswordForm.tsx
+++ frontend/src/components/PasswordForm.tsx
@@ -10,7 +10,8 @@ import { Label } from "@/components/ui/label";
 import { useState } from "react";
 
 export const PasswordForm = () => {
-	const [numberWords, setNumberWords] = useState<number[]>([3]);
+	const [numberWords, setNumberWords] = useState<number[]>([4]);
+
 	return (
 		<div className="flex justify-center mt-10">
 			<div className="grid grid-cols-2 gap-8 w-[600px]">
@@ -21,7 +22,7 @@ export const PasswordForm = () => {
 					<Slider
 						id="numberWords"
 						className="mt-4"
-						defaultValue={[3]}
+						defaultValue={[4]}
 						min={3}
 						max={7}
 						step={1}
@@ -30,16 +31,22 @@ export const PasswordForm = () => {
 				</div>
 				<div className="flex-col flex text-center justify-center">
 					<Label htmlFor="numberWords" className="mb-5">
-						Elige tu delimitador
+						Elige como se va a separar tu contraseña
 					</Label>
-					<Select>
+					<Select onValueChange={(value) => console.info(value)}>
 						<SelectTrigger className="">
-							<SelectValue />
+							<SelectValue placeholder="Elige tu delimitador" />
 						</SelectTrigger>
 						<SelectContent>
-							<SelectItem value="light">Light</SelectItem>
-							<SelectItem value="dark">Dark</SelectItem>
-							<SelectItem value="system">System</SelectItem>
+							<SelectItem value="space">Espacio</SelectItem>
+							<SelectItem value="comma">Coma</SelectItem>
+							<SelectItem value="dash">Diagonal</SelectItem>
+							<SelectItem value="underscore">Guion Bajo</SelectItem>
+							<SelectItem value="period">Punto</SelectItem>
+							<SelectItem value="slash">Slash</SelectItem>
+							<SelectItem value="pipe">Pipe</SelectItem>
+							<SelectItem value="PascalCase">PascalCase</SelectItem>
+							<SelectItem value="camelCase">camelCase</SelectItem>
 						</SelectContent>
 					</Select>
 				</div>
