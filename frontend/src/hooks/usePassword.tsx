3204f665572b777e7d91bad58ad166ff58953ebc
Diego Armando <dgox16@gmail.com>
2024-10-18 19:05:29 -0600

    Se agregaron los paquetes para el deploy del front

--- frontend/src/hooks/usePassword.tsx
+++ frontend/src/hooks/usePassword.tsx
@@ -1,42 +1,52 @@
-import { PasswordRequest, HandleChangeType, UsePasswordReturn } from '@/types/FormTypes';
-import axios from 'axios';
-import { useToast } from "@/hooks/use-toast"
+import {
+	PasswordRequest,
+	HandleChangeType,
+	UsePasswordReturn,
+} from "@/types/FormTypes";
+import axios from "axios";
+import { useToast } from "@/hooks/use-toast";
 
+import { useEffect, useRef, useState } from "react";
+import { useLanguageStore } from "@/store/languageStore";
 
-import { useEffect, useRef, useState } from 'react';
-import { useLanguageStore } from '@/store/languageStore';
+export const usePassword = (
+	initialValues: PasswordRequest,
+): UsePasswordReturn => {
+	const { toast } = useToast();
+	const { isSpanish } = useLanguageStore();
+	const [formData, setFormData] = useState<PasswordRequest>(initialValues);
+	const [password, setPassword] = useState("");
+	const isFirstRender = useRef(true);
 
+	const handleChange: HandleChangeType = (name, value) => {
+		setFormData((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+	};
 
-export const usePassword = (initialValues: PasswordRequest): UsePasswordReturn => {
-    const { toast } = useToast()
-    const { isSpanish } = useLanguageStore();
-    const [formData, setFormData] = useState<PasswordRequest>(initialValues);
-    const [password, setPassword] = useState("")
-    const isFirstRender = useRef(true);
+	useEffect(() => {
+		const getPassword = async () => {
+			const { data } = await axios.post(
+				`${import.meta.env.VITE_API_URL}/es/password`,
+				formData,
+			);
+			if (data.status) {
+				setPassword(data.password);
+				toast({
+					description: isSpanish
+						? "Nueva contraseña creada."
+						: "New password created.",
+				});
+			}
+		};
+		if (isFirstRender.current) {
+			isFirstRender.current = false;
+			return;
+		}
+		getPassword();
+	}, [formData]);
 
-    const handleChange: HandleChangeType = (name, value) => {
-        setFormData((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
-
-    useEffect(() => {
-        const getPassword = async () => {
-            const { data } = await axios.post("http://localhost:8000/es/password", formData)
-            if (data.status) {
-                setPassword(data.password)
-                // toast({
-                //     description: isSpanish ? "Nueva contraseña creada." : "New password created.",
-                // })
-            }
-        }
-        if (isFirstRender.current) {
-            isFirstRender.current = false;
-            return;
-        }
-        getPassword()
-    }, [formData]);
-
-    return { formData, handleChange, password };
+	return { formData, handleChange, password };
 };
+
