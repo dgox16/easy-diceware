0b0790c66e16a6e6b7e7c5e97615ee5df2e33d65
Diego Armando <dgox16@gmail.com>
2024-10-18 19:24:25 -0600

    Mejor diseño movil

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,46 +3,41 @@ import { PasswordAnimate } from "@/components/PasswordAnimate.tsx";
 import { PasswordForm } from "@/components/PasswordForm.tsx";
 import { usePassword } from "./hooks/usePassword";
 import { PasswordSpace } from "./components/PasswordSpace";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 import { SectionInformation } from "./components/SectionInformation";
 import { useLanguageStore } from "./store/languageStore";
 import { useLanguage } from "./hooks/useLanguage";
 
 const App = () => {
-	useLanguage()
-	const { formData, handleChange, password } = usePassword({ count: 4, type: "space" })
+	useLanguage();
+	const { formData, handleChange, password } = usePassword({
+		count: 4,
+		type: "space",
+	});
 	const { isSpanish } = useLanguageStore();
 
 	return (
 		<MainLayout>
 			<div className="flex justify-center mt-10 sm:mt-28 flex-col">
 				<div className="flex justify-center text-center">
-					<h1 className="scroll-m-20 text-[1.8rem] sm:text-6xl -mb-1 sm:mb-1 font-black tracking-tight">
-						{isSpanish
-							? "La clave es la simplicidad"
-							: "The key is simplicity"
-						}
+					<h1 className="scroll-m-20 text-[1.85rem] sm:text-6xl -mb-1 sm:mb-1 font-black tracking-tight">
+						{isSpanish ? "La clave es la simplicidad" : "The key is simplicity"}
 					</h1>
 				</div>
 				<div className="flex justify-center text-center">
 					<h2 className="scroll-m-20 text-base sm:text-2xl font-semibold tracking-tight">
 						{isSpanish
 							? "Fácil de recordar, imposible de hackear"
-							: "Easy to remember, impossible to hack"
-						}
+							: "Easy to remember, impossible to hack"}
 					</h2>
 				</div>
-				{password ? (
-					<PasswordSpace password={password} />
-				) : (
-					<PasswordAnimate />
-				)}
+				{password ? <PasswordSpace password={password} /> : <PasswordAnimate />}
 				<PasswordForm handleChange={handleChange} formData={formData} />
 			</div>
 			<SectionInformation />
 			<Toaster />
-		</MainLayout >
+		</MainLayout>
 	);
-}
+};
 
 export default App;
--- frontend/src/components/PasswordForm.tsx
+++ frontend/src/components/PasswordForm.tsx
@@ -10,22 +10,27 @@ import { Label } from "@/components/ui/label";
 import { PasswordFormProps } from "@/types/FormTypes";
 import { useLanguageStore } from "@/store/languageStore";
 
-export const PasswordForm: React.FC<PasswordFormProps> = ({ formData, handleChange }) => {
+export const PasswordForm: React.FC<PasswordFormProps> = ({
+	formData,
+	handleChange,
+}) => {
 	const { isSpanish } = useLanguageStore();
 
 	return (
 		<div className="flex justify-center mt-7 sm:mt-10 mx-5">
 			<div className="grid grid-cols-2 gap-8 w-[600px]">
 				<div className="flex-col col-span-2 sm:col-span-1 flex text-center">
-					<Label htmlFor="numberWords" className="mb-2 sm:mb-5 font-semibold text-xs sm:text-base">
+					<Label
+						htmlFor="numberWords"
+						className="mb-2 sm:mb-5 font-semibold text-xs sm:text-base"
+					>
 						{isSpanish
 							? `Tu contraseña tendra ${formData.count} palabras:`
-							: `Your password will have ${formData.count} words:`
-						}
+							: `Your password will have ${formData.count} words:`}
 					</Label>
 					<Slider
 						id="numberWords"
-						className="mt-4"
+						className="mt-2 sm:mt-4 w-3/4 mx-auto sm:mx-0 sm:w-full"
 						defaultValue={[4]}
 						min={3}
 						max={6}
@@ -34,24 +39,46 @@ export const PasswordForm: React.FC<PasswordFormProps> = ({ formData, handleChan
 					/>
 				</div>
 				<div className="flex-col col-span-2 sm:col-span-1 sm:flex text-center justify-center">
-					<Label htmlFor="numberWords" className="mb-5 font-semibold text-xs sm:text-base">
+					<Label
+						htmlFor="numberWords"
+						className="mb-5 font-semibold text-xs sm:text-base"
+					>
 						{isSpanish
 							? "Elige el separador de tu contraseña:"
-							: "Choose your password separator:"
-						}
+							: "Choose your password separator:"}
 					</Label>
 					<Select onValueChange={(value) => handleChange("type", value)}>
-						<SelectTrigger className="mt-3 sm:mt-0">
-							<SelectValue placeholder={isSpanish ? "Selecciona tu delimitador" : "Select your delimiter"} />
+						<SelectTrigger className="mt-3 sm:mt-0 w-3/4 mx-auto sm:mx-0 sm:w-full">
+							<SelectValue
+								placeholder={
+									isSpanish
+										? "Selecciona tu delimitador"
+										: "Select your delimiter"
+								}
+							/>
 						</SelectTrigger>
 						<SelectContent>
-							<SelectItem value="space">{isSpanish ? "Espacio" : "Space"}</SelectItem>
-							<SelectItem value="comma">{isSpanish ? "Coma" : "Comma"}</SelectItem>
-							<SelectItem value="dash">{isSpanish ? "Guion" : "Dash"}</SelectItem>
-							<SelectItem value="underscore">{isSpanish ? "Guion Bajo" : "Underscore"}</SelectItem>
-							<SelectItem value="period">{isSpanish ? "Punto" : "Period"}</SelectItem>
-							<SelectItem value="slash">{isSpanish ? "Diagonal" : "Slash"}</SelectItem>
-							<SelectItem value="pipe">{isSpanish ? "Pipe" : "Pipe"}</SelectItem>
+							<SelectItem value="space">
+								{isSpanish ? "Espacio" : "Space"}
+							</SelectItem>
+							<SelectItem value="comma">
+								{isSpanish ? "Coma" : "Comma"}
+							</SelectItem>
+							<SelectItem value="dash">
+								{isSpanish ? "Guion" : "Dash"}
+							</SelectItem>
+							<SelectItem value="underscore">
+								{isSpanish ? "Guion Bajo" : "Underscore"}
+							</SelectItem>
+							<SelectItem value="period">
+								{isSpanish ? "Punto" : "Period"}
+							</SelectItem>
+							<SelectItem value="slash">
+								{isSpanish ? "Diagonal" : "Slash"}
+							</SelectItem>
+							<SelectItem value="pipe">
+								{isSpanish ? "Pipe" : "Pipe"}
+							</SelectItem>
 							<SelectItem value="PascalCase">PascalCase</SelectItem>
 							<SelectItem value="camelCase">camelCase</SelectItem>
 						</SelectContent>
