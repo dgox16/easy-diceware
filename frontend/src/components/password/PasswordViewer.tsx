97788e6423937431e7641b96643bc824f3c6797b
Diego Armando <dgox16@gmail.com>
2024-10-21 19:30:47 -0600

    Mejoras de margen

--- frontend/src/components/password/PasswordViewer.tsx
+++ frontend/src/components/password/PasswordViewer.tsx
@@ -11,7 +11,7 @@ export const PasswordViewer: React.FC<{ password: PasswordResponse }> = ({
 			<div className="text-center text-xs sm:text-lg w-full sm:w-[600px] mx-auto bg-zinc-200 dark:bg-zinc-800 rounded-lg py-3 px-1 font-semibold text-zinc-900 dark:text-zinc-100">
 				{password.password}
 			</div>
-			<span className="mx-auto mt-3 text-sm sm:text-base">
+			<span className="mx-auto mt-4 text-sm sm:text-base">
 				{isSpanish
 					? "Una computadora tardaría aproximadamente"
 					: "It would take a computer about"}{" "}
