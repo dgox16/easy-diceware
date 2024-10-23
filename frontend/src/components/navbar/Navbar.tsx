4042a4310da191f0d0bb2c4a5a8dafd2c0406fac
Diego Gomez <Diego.Gomez@pilsac.com>
2024-10-23 17:20:36 -0600

    Primer fase del diseno para la pagina de analizar contrasenas

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -2,15 +2,27 @@ import { buttonVariants } from "@/components/ui/button.tsx";
 import { Github } from "lucide-react";
 import { ButtonToggleTheme } from "@/components/navbar/ButtonToggleTheme";
 import { ButtonToggleLanguage } from "@/components/navbar/ButtonToggleLanguage";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
 		<nav className="flex justify-center items-center h-14 mx-5 sm:mx-0">
 			<div className="w-[1000px]">
 				<div className="flex justify-between items-center">
-					<h1 className="scroll-m-20 text-lg font-semibold tracking-tight">
-						EasyDiceware
-					</h1>
+					<div>
+						<Link
+							to={"/"}
+							className="scroll-m-20 text-lg font-semibold tracking-tight"
+						>
+							EasyDiceware
+						</Link>
+						<Link
+							to={"/check-password"}
+							className="text-sm ml-6 text-zinc-700 dark:text-zinc-300 hover:underline hidden sm:inline"
+						>
+							Check my password
+						</Link>
+					</div>
 					<div>
 						<a
 							className={`${buttonVariants({ variant: "outline", size: "icon" })} mr-2`}
--- frontend/src/pages/CheckPasswordPage.tsx
+++ frontend/src/pages/CheckPasswordPage.tsx
@@ -1,6 +1,7 @@
 import { MainLayout } from "@/layouts/MainLayout.tsx";
 import { useLanguageStore } from "@/store/languageStore";
 import { PrincipalText, SecondaryText } from "@/components/text/HeaderText";
+import { Input } from "@/components/ui/input";
 
 export const CheckPasswordPage = () => {
 	const { isSpanish } = useLanguageStore();
@@ -10,15 +11,29 @@ export const CheckPasswordPage = () => {
 			<div className="flex justify-center mt-10 sm:mt-28 flex-col">
 				<div className="flex justify-center text-center">
 					<PrincipalText>
-						{isSpanish ? "La clave es la simplicidad" : "The key is simplicity"}
+						{isSpanish ? "Mide tu seguridad" : "Measure your security"}
 					</PrincipalText>
 				</div>
 				<div className="flex justify-center text-center">
 					<SecondaryText>
 						{isSpanish
-							? "Fácil de recordar, imposible de hackear"
-							: "Easy to remember, impossible to hack"}
+							? "¿Quieres saber cómo de segura es tu contraseña?"
+							: "Do you want to know how secure is your password?"}
 					</SecondaryText>
+				</div>
+
+				<div className="flex justify-center mt-5 mx-5">
+					<div className="w-[600px]">
+						<Input
+							type="email"
+							placeholder={
+								isSpanish
+									? "Coloca la contraseña a analizar"
+									: "Type the password to be analyzed"
+							}
+							className="text-center text-lg py-6 bg-zinc-200 dark:bg-zinc-800 rounded-lg px-1 font-semibold text-zinc-900 dark:text-zinc-100"
+						/>
+					</div>
 				</div>
 			</div>
 		</MainLayout>
