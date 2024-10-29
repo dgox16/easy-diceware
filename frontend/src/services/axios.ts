83b49cc44864b186216b989af0f2cffcf0850d9b
Diego Gomez <Diego.Gomez@pilsac.com>
2024-10-29 15:51:57 -0600

    Se vuelven a usar env para la url de la api

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://api-easy-diceware.up.railway.app",
+	baseURL: import.meta.env.VITE_API_URL,
 	withCredentials: true,
 	withXSRFToken: true,
 });
